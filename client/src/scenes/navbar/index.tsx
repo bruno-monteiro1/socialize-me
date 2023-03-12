fa4b11bf182b2cd389bdb8c4006024091db5744b
Bruno Monteiro <brunobapmon@gmail.com>
2023-03-12 12:15:58 -0300

    feat: login and register pages

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -40,7 +40,7 @@ const NavBar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `Roger`;
+  const fullName = user.firstName;
 
   return (
     <FlexBetween padding="1rem 6%" bgcolor={alt}>
